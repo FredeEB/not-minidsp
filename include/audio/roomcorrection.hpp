0740e81e174bf1cac38cae70adfe815fd054bb96
Frede Braendstrup <frederikbraendstrup@gmail.com>
2020-09-24 11:04:21 +0000

    conformed roomcorrection to algorithm concept

--- include/audio/roomcorrection.hpp
+++ include/audio/roomcorrection.hpp
@@ -18,13 +18,14 @@
 
 namespace Audio {
 
-template <typename T, std::size_t FramesPerBuffer = 64, std::size_t Channels = 2>
+template <typename BufferType, std::size_t Channels>
 class RoomCorrection {
 public:
-    using value_type = T;
-    using filter_type = Biquad<value_type, FramesPerBuffer>;
+    using value_type = typename BufferType::value_type;
+    using buffer_type = BufferType;
+    using filter_type = Biquad<buffer_type>;
 
-    void process(std::array<value_type, FramesPerBuffer>& buffer) {
+    void process(buffer_type& buffer) {
         for (auto& filter : filters) filter.process(buffer);
     }
 
