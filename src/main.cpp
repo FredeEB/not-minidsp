22f558ea187e2e1f46b69ea9d93b05c2c9a809c0
Frede Braendstrup <frederikbraendstrup@gmail.com>
2020-09-24 11:12:18 +0000

    added simple example

--- src/main.cpp
+++ src/main.cpp
@@ -8,36 +8,22 @@
 #include <util/singleton.hpp>
 #include <audio/audioprocessor.hpp>
 #include <audio/roomcorrection.hpp>
+#include <audio/sine.hpp>
+// #include <audio/audiostream.hpp>
 
-static const std::size_t FramesPerBuffer = 64;
+using namespace std::literals::chrono_literals;
+
+static const std::size_t FramesPerBuffer = 512;
 static const std::size_t Channels = 2;
 
-void setup() {
-    fmt::print("Running setup...\n");
-    auto& config = Util::Singleton<Util::Config>();
-
-    print_devices();
-    config.SampleRate = 48000.0;
-
-    fmt::print("Which card to use?\n");
-    std::cin >> config.deviceIndex;
-
-    fmt::print("------------\n");
-    fmt::print("Device: {}\n", portaudio::System::instance().deviceByIndex(config.deviceIndex).name());
-    fmt::print("Channels: {}\n", Channels);
-    fmt::print("Frames per buffer: {}\n", FramesPerBuffer);
-    fmt::print("Samplerate: {}\n", config.SampleRate);
-    fmt::print("------------\n");
-}
-
 int main() {
-    portaudio::AutoSystem system;
-    setup();
+    Audio::AudioProcessor<Audio::RoomCorrection> processor;
 
-    Audio::AudioProcessor<Audio::RoomCorrection<float>> processor;
-
-    processor.algo().registerFilter({1, 0.2f, 1, 1, 1});
+    processor.algo().registerFilter({-1.96297470f, 0.96364759f, 0.000168223247f, 0.000336446494f, 0.000168223247f});
+    processor.algo().registerFilter({-1.81531792f, 0.83098222f, 0.00391608f, 0.00783215f, 0.00391608f});
+    fmt::print("Running!\n");
     processor.run();
+    std::this_thread::sleep_for(5s);
 
     return 0;
 }
