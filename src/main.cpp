ea35ede3c692699f33b7e847c797ac46634b0967
Frede Braendstrup <frederikbraendstrup@gmail.com>
2020-10-01 23:20:08 +0000

    added default program using biquads

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <audio/audioprocessor.hpp>
 #include <audio/roomcorrection.hpp>
 #include <audio/sine.hpp>
+#include <audio/biquad.hpp>
 
 using namespace std::literals::chrono_literals;
 
@@ -12,7 +13,7 @@ static const std::size_t FramesPerBuffer = 512;
 static const std::size_t Channels = 2;
 
 int main() {
-    Audio::AudioProcessor<Audio::RoomCorrection> processor;
+    Audio::AudioProcessor<Audio::RoomCorrection, Audio::BiquadTag> processor;
 
     processor.algo().registerFilter({-1.96297470f, 0.96364759f, 0.000168223247f, 0.000336446494f, 0.000168223247f});
     processor.algo().registerFilter({-1.81531792f, 0.83098222f, 0.00391608f, 0.00783215f, 0.00391608f});
