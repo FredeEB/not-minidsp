7a0b63b1f76066eea54429f2dab062b61e65c01c
Frede Braendstrup <frederikbraendstrup@gmail.com>
2020-10-01 23:22:45 +0000

    added default program using FIR

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,7 @@
 #include <audio/audioprocessor.hpp>
 #include <audio/roomcorrection.hpp>
 #include <audio/sine.hpp>
+#include <audio/fir.hpp>
 
 using namespace std::literals::chrono_literals;
 
@@ -12,10 +13,8 @@ static const std::size_t FramesPerBuffer = 512;
 static const std::size_t Channels = 2;
 
 int main() {
-    Audio::AudioProcessor<Audio::RoomCorrection> processor;
+    Audio::AudioProcessor<Audio::RoomCorrection, Audio::FIRTag> processor(1024);
 
-    processor.algo().registerFilter({-1.96297470f, 0.96364759f, 0.000168223247f, 0.000336446494f, 0.000168223247f});
-    processor.algo().registerFilter({-1.81531792f, 0.83098222f, 0.00391608f, 0.00783215f, 0.00391608f});
     processor.run();
     std::this_thread::sleep_for(5s);
 
