77ac9801b0a4437ad7e16c923bc400e9cd4bac5d
Frede Braendstrup <frederikbraendstrup@gmail.com>
2020-10-03 13:15:21 +0000

    [WIP] Updated implementation of FIR Filter

--- include/audio/fir.hpp
+++ include/audio/fir.hpp
@@ -1,11 +1,15 @@
 #ifndef FIR_H
 #define FIR_H
 
+#include <algorithm>
+#include <iterator>
 #include <vector>
 #include <istream>
 #include <numeric>
+#include <execution>
 
 #include <audio/filter.hpp>
+#include <audio/circularIterator.hpp>
 
 namespace Audio {
 
@@ -14,7 +18,7 @@ struct FIRTag {};
 template <typename BufferType>
 using FIRFilter = Filter<BufferType, FIRTag>;
 
-template <typename BufferType, std::size_t Taps>
+template <typename BufferType>
 std::istream& operator>>(std::istream&, FIRFilter<BufferType>&);
 
 template <typename BufferType>
@@ -24,23 +28,40 @@ public:
     using value_type = typename buffer_type::value_type;
     using delayline_type = std::vector<value_type>;
 
-    constexpr Filter(std::size_t taps) : coeffiecients(taps), delayline(taps) {}
+    constexpr Filter(std::size_t taps) : coefficients(taps), delayline(taps) {}
 
     void process(buffer_type& buffer) {
-        for (auto bufferit = buffer.begin(); bufferit != buffer.end(); ++bufferit, ++delaylinehead) {
-            if (delaylinehead == delayline.end()) delaylinehead = delayline.begin();
-
-            *delaylinehead = *bufferit;
-
-            *bufferit = std::transform_reduce(coeffiecients.begin(), coeffiecients.end(), delaylinehead, 0.0f);
-        }
+        std::transform(buffer.begin(), buffer.end(), buffer.begin(),
+                       [&](auto sample) { return calculateSample(sample); });
     }
 
+    friend std::istream& operator>><buffer_type>(std::istream&, Filter&);
+
 private:
-    delayline_type coeffiecients;
+    inline value_type calculateSample(value_type value) {
+        if (delaylinehead == std::prev(delayline.begin())) delaylinehead = std::prev(delayline.end());
+
+        *delaylinehead = value;
+
+        value_type res = std::transform_reduce(std::execution::par, coefficients.begin(), coefficients.end(),
+                                               circular_iterator(delayline, delaylinehead), 0.0f);
+
+        --delaylinehead;
+
+        return res;
+    }
+
+    delayline_type coefficients;
     delayline_type delayline;
     typename delayline_type::iterator delaylinehead = delayline.begin();
 };
+
+template <typename BufferType>
+std::istream& operator>>(std::istream& is, FIRFilter<BufferType>& filter) {
+    std::istream_iterator<typename BufferType::value_type> begin(is), end;
+    std::copy(begin, end, std::back_inserter(filter.coefficients));
+    return is;
+}
 
 } // namespace Audio
 
