166e1ab50fbad7958a40a88320ce0e1db7f38c17
fredeb <frederikbraendstrup@gmail.com>
2020-10-08 09:37:44 +0200

    added Systemtraits as a collective way of configuring the system

---
+++ include/audio/processortraits.hpp
@@ -0,0 +1,19 @@
+#ifndef PROCESSORTRAITS_H
+#define PROCESSORTRAITS_H
+
+#include <bits/c++config.h>
+#include <array>
+#include <tuple>
+#include "util/repeat_type.hpp"
+template <typename ValueType, std::size_t ChannelSize, std::size_t Channels>
+struct SystemTraits {
+    using value_type = ValueType;
+    using channel_type = std::array<ValueType, ChannelSize>;
+    using buffer_type = typename Util::repeat_type<channel_type, Channels, std::tuple>::type;
+    static constexpr auto channels = Channels;
+    static constexpr auto channel_size = ChannelSize;
+    static constexpr auto frame_size = channels * channel_size;
+    static_assert(frame_size > 0, "Framesize can't be 0, both channels and channel_size has to be greater than 0");
+};
+
+#endif /* PROCESSORTRAITS_H */
